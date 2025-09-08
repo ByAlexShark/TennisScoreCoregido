d4fc37fa90cc60ce2958f78ac68c0769270b9013
ByAlexShark <alexanderzafiro01@gmail.com>
2025-09-08 09:45:54 -0400

    3ra prueba el jugador 2 anota tres a 0

--- src/tennisScorer.spec.js
+++ src/tennisScorer.spec.js
@@ -1,11 +1,13 @@
 import { TennisScorer } from "./tennisScorer.js";
 
 describe("TennisScorer", () => {
+  //PRUEBA DE INICIO 0-0
     it("game empieza con ambos en cero", () => {
         const score = new TennisScorer(); 
         const result = score.showScore(); 
         expect(result).toEqual("Love - Love");
     });
+    //PRUEBAS PLAYER 1
     it("player 1 anota un punto y el jugador no anota, 1-0", () => {
          const score = new TennisScorer(); 
         score.player1Scores();            
@@ -28,6 +30,7 @@ describe("TennisScorer", () => {
         expect(score.showScore()).toEqual("40 - Love");
     });
     
+    //PRUEBAS PARA PLAYER 2
     it("player 2 anota su primer punto y el jugador 2 no anota 0 - 1 ", () => {
         const score = new TennisScorer();
         score.player2Scores();
@@ -41,4 +44,11 @@ describe("TennisScorer", () => {
         expect(score.showScore()).toEqual("Love - 30");
     });
 
+    it("player 2 anota tres puntos y el jugador 2 no anota 0 - 3 ", () => {
+        const score = new TennisScorer();
+        score.player2Scores();
+        score.player2Scores();
+        score.player2Scores();
+        expect(score.showScore()).toEqual("Love - 40");
+    });
 });
